5608ea326b76eac912dc1ff243e30e3116ffed6a
Jaegar Saunders <jaegarsaunders@192.168.219.23>
2023-09-19 19:12:31 -0230

    Added states to the input field that collects data from user

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,53 @@
 import React from "react";
 import "../App.css";
+import { useState } from "react";
 import { Input, InputGroup, InputRightElement, Button } from "@chakra-ui/react";
 // Import api
 import alchemy from "../apis/alchemyapi";
 // Import API tools
+import { fromHex } from "alchemy-sdk";
 
 
 
-function handleClick() {
-  const nfts = alchemy.nft.getNftsForOwner("vitalik.eth");
-  console.log(nfts);
-}
 export default function Header() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (e) => {
+    // Step 3: Update state when input value changes
+    setInputValue(e.target.value);
+  };
+
+  async function handleClick() {
+    // Get inputted address
+  
+    console.log(inputValue);
+    setInputValue("");
+  
+  
+  
+    // // Contract address ( Will need to set this to user inputted address)
+    // const address = ["0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"];
+    // // Get all NFTs
+    // const response = await alchemy.core.getAssetTransfers({
+    //   fromBlock: "0x0",
+    //   contractAddresses: address,
+    //   category: ["erc721"],
+    //   excludeZeroValue: false,
+    // });
+    
+  
+  
+    // // Will need to set this NFT ID to the one that is inputted
+    // const nftId = 3;
+  
+    // // Get transactions for the NFT
+    // let txns = response.transfers.filter(
+    //   (txn) => fromHex(txn.erc721TokenId) === nftId
+    // );
+    // // Eventually instead of console logging we will show the transactions on the page
+    // console.log(txns);
+  }
+
   return (
     <div className="header">
       <h1>
@@ -23,15 +59,24 @@ export default function Header() {
       </p>
       <InputGroup size="md">
         <Input
-          placeholder="NFT Address"
+          placeholder="Collection Contact Address"
           pr="4.5rem"
           size="lg"
           variant="Filled"
           focusBackgroundColor="red"
+          value={inputValue} 
+          onChange={handleChange} 
         />
-        <InputRightElement width="4.5rem" height="100%" display="flex" justifyContent="center" alignItems="center" marginRight="0.5rem">
-          <Button h="1.75rem" size="sm" onClick={handleClick()}>
-            Search
+        <InputRightElement
+          width="4.5rem"
+          height="100%"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          marginRight="0.5rem"
+        >
+          <Button h="1.75rem" size="sm" onClick={handleClick}>
+            Next
           </Button>
         </InputRightElement>
       </InputGroup>
